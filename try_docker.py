f0109a50a5bd05314170181d7a0b1d8e60528f4d
gaozheng@joygames.cc <gaozheng@joygames.cc>
2025-08-03 22:26:22 +0800

    add try docker script

--- try_docker.py
+++ try_docker.py
@@ -1,30 +1,42 @@
 import os
 
-# 打印所有Docker相关的环境变量
+# Enable HyperV instead of running docker with WSL2 on Windows 10
 docker_env = {k: v for k, v in os.environ.items() if k.startswith('DOCKER_')}
-print("Docker相关环境变量:", docker_env)
+print("Docker env vars:", docker_env)
 
 import docker
 from docker.utils import utils
 
-# 查看自动检测的连接地址
 try:
     auto_detected_url = utils.parse_host(None)  # 模拟自动检测过程
-    print(f"自动检测的Docker连接地址: {auto_detected_url}")
+    print(f"auto detected docker host address: {auto_detected_url}")
 except Exception as e:
-    print(f"自动检测过程出错: {e}")
+    print(f"auto detect docker host address error: {e}")
 
 import docker
 
-# 推荐使用环境变量自动配置
-client = docker.from_env()
+# recommend to use env var to config docker client
+# client = docker.from_env()
 
-# 测试连接
+base_url = 'tcp://localhost:2375'
+# base_url = 'tcp://172.26.224.1:2375'
+client = docker.DockerClient(base_url=base_url)
+# test connection
 try:
-    print(client.version())  # 打印Docker版本信息
+    print("docker version:")
+    print(client.version())
 except docker.errors.APIError as e:
-    print(f"API错误: {e}")
+    print(f"API Error: {e}")
 except Exception as e:
-    print(f"连接错误: {e}")
-# 测试连接
-print(client.containers.list())
+    print(f"Connection Error: {e}")
+
+print("show all containers:")
+containers = client.containers.list()
+for container in containers:
+    print(container.name)
+
+images = client.images.list()
+# show all images
+print("show all images:")
+for image in images:
+    print(image.tags)
